1cbb08bc91d94c4616627901473b10c1ecc3e4b1
AinazZ <ziyazova.aynaz@mail.ru>
2020-03-08 17:54:23 +0300

    http-запрос и парсинг JSON

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,76 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Product> fetchProduct() async {
+  final response = await http.get('http://ostest.whitetigersoft.ru/api/common/product/list?appKey=phynMLgDkiG06cECKA3LJATNiUZ1ijs-eNhTf0IGq4mSpJF3bD42MjPUjWwj7sqLuPy4_nBCOyX3-fRiUl6rnoCjQ0vYyKb-LR03x9kYGq53IBQ5SrN8G1jSQjUDplXF');
+
+  if (response.statusCode == 200) {
+    return Product.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load product');
+  }
+}
+
+class Product {
+  final int productId;
+  final String title;
+  final int price;
+
+  Product({this.productId, this.title, this.price});
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      productId: json['productId'],
+      title: json['title'],
+      price: json['price'],
+    );
+  }
+}
 
 void main() => runApp(App());
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  App({Key key}) : super(key: key);
 
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  Future<Product> futureProduct;
+
+  @override
+  void initState() {
+    super.initState();
+    futureProduct = fetchProduct();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Продукты',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Продукты'),
+        ),
+        body: Center(
+          child: FutureBuilder<Product>(
+            future: futureProduct,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data.title);
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
 }
