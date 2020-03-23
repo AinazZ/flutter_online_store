9c403228f3b3e5060254ea1327b6acf6ea2dfada
AinazZ <ziyazova.aynaz@mail.ru>
2020-03-23 16:07:22 +0300

    Каталог

--- lib/category.dart
+++ lib/category.dart
@@ -1,14 +1,17 @@
 class Category {
+  final int categoryId;
   final String title;
   final String imageurl;
 
   Category({
+    this.categoryId,
     this.title,
     this.imageurl
   });
 
   factory Category.fromJson(Map<String, dynamic> json) {
     return Category(
+      categoryId: json['categoryId'] as int,
       title: json['title'] as String,
       imageurl: json['imageurl'] as String,);
   }
