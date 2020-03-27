f5ebddbf1010dc9d4eda1f82f38c17b1b0e862ce
AinazZ <ziyazova.aynaz@mail.ru>
2020-03-27 18:25:27 +0300

    Правки виджетов

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutteronlinestore/category_page.dart';
-import 'package:flutteronlinestore/product_list_page.dart';
+import 'package:flutteronlinestore/category/category_page.dart';
+import 'package:flutteronlinestore/product/product_list_page.dart';
 
 void main() => runApp(App());
 
@@ -10,21 +10,22 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/':(BuildContext context) => CategoryPage(),
-        '/products':(BuildContext context) => ProductListPage()
-      },
-      onGenerateRoute: (routeSettings) {
-        var path = routeSettings.name.split('/');
+      home: ProductListPage(),
+      // initialRoute: '/',
+      // routes: {
+      //   '/':(BuildContext context) => CategoryPage(),
+      //   '/products':(BuildContext context) => ProductListPage()
+      // },
+      // onGenerateRoute: (routeSettings) {
+      //   var path = routeSettings.name.split('/');
 
-        if (path[1] == "products") {
-          return MaterialPageRoute(
-            builder: (context) => ProductListPage(categoryId:path[2]),
-            settings: routeSettings,
-          );
-        }
-      },
+      //   if (path[1] == "products") {
+      //     return MaterialPageRoute(
+      //       builder: (context) => ProductListPage(categoryId:path[2]),
+      //       settings: routeSettings,
+      //     );
+      //   }
+      // },
     );
   }
 }
